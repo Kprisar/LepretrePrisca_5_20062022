34b45fe8ace08fcdbdb191982f2fcd2cd9b4f432
Prisca <arobazstudio@gmail.com>
2022-06-27 11:52:26 +0200

    data

---
+++ front/js/script.js
@@ -0,0 +1,73 @@
+fetch ("http://localhost:3000/api/products")
+.then((res) => res.json())
+.then((data) => addProducts(data))
+ 
+
+
+function addProducts(data){
+    //const _id = data[0]._id
+    //const imageUrl = data[0].imageUrl
+    //const altTxt = data[0].altTxt
+    //const name = data[0].name
+    //const description = data[0].description
+
+    data.forEach((kanap) => {
+    const {_id, imageUrl, altTxt, name, description} = kanap
+    const anchor = makeAnchor(_id)
+    const article =  document.createElement("article")
+    const image = makeImage(imageUrl, altTxt)
+    const h3 = makeH3(name)
+    const p = makeParagraph(description)
+    appendElementToArticle(article, [image, h3, p])
+    appendArticleToAnchor(anchor, article)
+
+});   
+}
+
+function appendElementToArticle(article, array){
+    array.forEach((item) => {
+        article.appendChild(item)
+    })
+    //article.appendChild(image)
+    //article.appendChild(h3)
+    //article.appendChild(p)
+
+}
+
+function makeAnchor(_id){
+    const anchor = document.createElement("a")
+    anchor.href = "./product.html?id=" + _id
+    return anchor
+
+}
+
+function appendArticleToAnchor(anchor, article) {
+    const items = document.querySelector("#items")
+    if (items != null) {
+        items.appendChild(anchor)
+        anchor.appendChild(article)
+}
+}
+
+function makeImage(imageUrl, altTxt) {
+    const image = document.createElement("img")
+    image.src = imageUrl
+    image.alt = altTxt
+    return image
+}
+
+
+function makeH3(name){
+    const h3 = document.createElement("h3")
+    h3.textContent = name
+    h3.classList.add("productName")
+    return h3
+}
+
+function makeParagraph(description){
+    const p = document.createElement("p")
+    p.textContent = description
+    p.classList.add("productDescription")
+    return p
+
+}
